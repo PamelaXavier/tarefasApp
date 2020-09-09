08164b325b2a0313da40483c5feb396fe4315855
PamelaXavier <pamela.xavier2@etec.sp.gov.br>
2020-09-09 15:45:25 -0300

    login
    login e manter logado

--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -35,4 +35,29 @@ export class UsuariosService {
     return await this.armazenamentoService.salvarDados('usuarios', this.listaUsuarios);
 
   }
+
+  public async login(email: string, senha: string){
+    let usuario: Usuario;
+
+    await this.buscarTodos();
+
+    const listaTemporaria = this.listaUsuarios.filter(usuarioArmazenado =>{
+      return (usuarioArmazenado.email == email && usuarioArmazenado.senha == senha);
+    } ); // esse método retorna um array;
+
+    if(listaTemporaria.length > 0) {
+      usuario = listaTemporaria.reduce(item => item);
+    }
+
+    return usuario;
+  }
+
+  public salvarUsuarioLogado(usuario: Usuario){
+    delete usuario.senha;
+    this.armazenamentoService.salvarDados('usuarioLogado', usuario);
+  }
+
+  public async buscarUsuarioLogado(){
+    return await this.armazenamentoService.pegarDados('usuarioLogado');
+  }
 }
